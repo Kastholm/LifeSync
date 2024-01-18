a95a184f1320f7ba1d0345b50e7b815a2b1500be
Marc <marc@aguardio.com>
2024-01-18 17:30:26 +0900

    ha fix

--- src/pages/Homeassistant.tsx
+++ src/pages/Homeassistant.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Homeassistant() {
   return (
     <div>
-     <iframe className='w-[95vw] min-h-screen' title="test" src='http://homeassistant.local:8123/lovelace-shroom/0' />
+     <iframe className='w-[95vw] min-h-screen' title="test" src='https://lifesync.us' />
     </div>
   )
 }
